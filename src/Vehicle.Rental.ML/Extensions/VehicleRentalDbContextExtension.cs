53a928b8d0896e2d6373e0c2fc061b3612e0113f
Jay Jha <jayjha040395@gmail.com>
2023-12-13 20:44:37 +0530

    Adding entity framework support

---
+++ src/Vehicle.Rental.ML/Extensions/VehicleRentalDbContextExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vehicle.Rental.DAL.Context;
+
+namespace Vehicle.Rental.DAL.Extensions
+{
+    public static class VehicleRentalDbContextExtension
+    {
+        public static IServiceCollection AddVehicleRentalContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddDbContext<VehicleRentalDbContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+            
+            });
+        }
+    }
+}
